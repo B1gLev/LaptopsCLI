08167bb343e0f6c1e1600be4266f7ca6a5f4234b
B1gLev <kozma.lev2004@gmail.com>
2025-05-19 19:04:59 +0200

    Add project files.

---
+++ LaptopsCLI/Laptop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopsCLI
+{
+    internal class Laptop
+    {
+        public Category Category { get; set; }
+        public string CPU { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string OS { get; set; }
+        public double Price { get; set; }
+        public int RAM { get; set; }
+        public string Screen { get; set; }
+        public string Storage { get; set; }
+
+        public Laptop(string row)
+        {
+            var data = row.Split(";");
+            Category = new(int.Parse(data[0]), data[1]);
+            CPU = data[2];
+            Manufacturer = new(int.Parse(data[3]), data[4]);
+            Model = data[5];
+            OS = data[6];
+            Price = double.Parse(data[7]);
+            RAM = int.Parse(data[8]);
+            Screen = data[9];
+            Storage = data[10];
+        }
+
+        public override string? ToString() => $"{Category.CategoryName} | {Manufacturer.ManufacturerName} {Model} | {OS}";
+    }
+}
---
+++ LaptopsCLI/Program.cs
@@ -0,0 +1,24 @@
+﻿using LaptopsCLI;
+
+var laptops = new List<Laptop>();
+
+using var sr = new StreamReader(@"../../../src/laptops.txt");
+sr.ReadLine();
+while(!sr.EndOfStream) laptops.Add(new(sr.ReadLine()));
+
+Console.WriteLine("5. feladat - Kiírás");
+laptops.Select((k, i) => new { Index = i + 1, Key = k }).ToList()
+    .ForEach(x => Console.WriteLine($"{x.Index}. {x.Key} | {x.Key.Price * 4.12:00} HUF"));
+
+Console.WriteLine("\n6. feladat - Keresés");
+var intelCoreSeven = laptops.Where(x => x.CPU.Contains("Intel Core i7") && x.Storage.Contains("SSD")).Select((k, i) => new { Index = i + 1, Key = k }).ToList();
+intelCoreSeven.ForEach(x => Console.WriteLine($"[{x.Index}] {x.Key}"));
+Console.WriteLine($"A laptopok átlag ára: {intelCoreSeven.Average(x => x.Key.Price)} INR");
+
+Console.WriteLine("\n7. feladat - Faájlba írás");
+using var sw = new StreamWriter(@"../../../src/cheap.txt");
+laptops.Where(x => x.Category.CategoryName.Equals("Gaming")).OrderBy(x => x.Price).ToList().Take(20)
+    .ToList().ForEach(x => sw.WriteLine($"{x.Manufacturer.ManufacturerName} {x.Model}" +
+    $"\n\t{x.CPU}" +
+    $"\n\t{x.Storage}" +
+    $"\n\t{x.Screen}"));
